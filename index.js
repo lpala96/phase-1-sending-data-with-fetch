509cd83ad43855d302fe79b5c25ccfca7f9d0488
Loyda <palacioloydita@gmail.com>
2022-04-08 15:11:41 -0700

    commit tree 29f6af824e0cadf031bac7ce533780e2ab4bf462

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+
+function submitData(firstName, email) {
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch('http://localhost:3000/users', configObject)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => error(response))
+    
+    
+}
+
+function error (newError) {
+    document.body.append(newError)
+}
+
+function addNewContact(newID) {
+document.getElementsByTagName("body")[0].innerHTML = newID;
+
+}
+
+submitData("Name", "email")
